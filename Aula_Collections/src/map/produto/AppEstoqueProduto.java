4186d0fc7305c400f554ca55d04904002eda9782
FabioCantuario <fabiocantuario@hotmail.com>
2024-05-28 16:31:00 -0400

    Updated

--- Aula_Collections/src/map/produto/AppEstoqueProduto.java
+++ Aula_Collections/src/map/produto/AppEstoqueProduto.java
@@ -10,21 +10,21 @@ public class AppEstoqueProduto {
 		estoque.exibirProdutos();
 
 		// Adiciona produtos ao estoque
-		estoque.adicionarProduto(1L, "Notebook", 1, 1500.0);
-		estoque.adicionarProduto(2L, "Mouse", 5, 25.0);
-		estoque.adicionarProduto(3L, "Monitor", 10, 400.0);
-		estoque.adicionarProduto(4L, "Teclado", 2, 40.0);
+		estoque.adicionarProduto(123L, "caderno", 10, 10.0);
+		estoque.adicionarProduto(145L, "borracha", 100, 3.5);
+		estoque.adicionarProduto(155L, "lápis", 50, 5.0);
+		estoque.adicionarProduto(489L, "ResmaPapel", 20, 20.0);
 
 		// Exibe os produtos no estoque
 		estoque.exibirProdutos();
 
 		// Calcula e exibe o valor total do estoque
 		System.out.println("Valor total do estoque: R$" + estoque.calcularValorTotalEstoque());
-/*
+
 		// Obtém e exibe o produto mais caro
 		Produto produtoMaisCaro = estoque.obterProdutoMaisCaro();
 		System.out.println("Produto mais caro: " + produtoMaisCaro);
-		/*
+/*
 		 * // Obtém e exibe o produto mais barato Produto produtoMaisBarato =
 		 * estoque.obterProdutoMaisBarato(); System.out.println("Produto mais barato: "
 		 * + produtoMaisBarato);
